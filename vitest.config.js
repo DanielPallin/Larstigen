01dce45897ebd7d23b7a72047c5834bf5bb22fbe
StevenCesario <steven.cesario@proton.me>
2026-04-09 19:56:00 +0200

    corrected pattern

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     // Detta letar i ALLA mappar under tests efter filer som slutar på .test.js eller .test.ts
-    include: ['tests/*/.{test,spec}.{js,ts}'],
+    include: ['tests/**/*.{test,spec}.{js,ts}'],
     environment: 'jsdom', // Krävs för att testa kod som använder document.querySelector osv.
   },
 });
