6319746349bfbe32793350cf94f8fc893af203f0
StevenCesario <steven.cesario@proton.me>
2026-04-09 19:59:26 +0200

    exclude pattern för e2e mappen som playwright tar hand om

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     // Detta letar i ALLA mappar under tests efter filer som slutar på .test.js eller .test.ts
     include: ['tests/**/*.{test,spec}.{js,ts}'],
+    exclude: [...configDefaults.exclude, 'tests/e2e/**'], // Ignorera e2e-mappen så att Playwright får hantera de filerna ifred
     environment: 'jsdom', // Krävs för att testa kod som använder document.querySelector osv.
   },
 });
