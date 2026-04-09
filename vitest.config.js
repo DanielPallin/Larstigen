939275adfbb23c6e4e5a1f9ea9a2511762d7d9ba
StevenCesario <steven.cesario@proton.me>
2026-04-09 19:41:06 +0200

    jsdom för intergration tests inkluderat nu oxå

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
   test: {
     // Detta letar i ALLA mappar under tests efter filer som slutar på .test.js eller .test.ts
     include: ['tests/*/.{test,spec}.{js,ts}'],
+    environment: 'jsdom', // Krävs för att testa kod som använder document.querySelector osv.
   },
 });
